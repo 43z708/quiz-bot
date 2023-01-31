d85d67342d8a4dd43b4c24fecb910fb2148e1ac5
43z708 <s7080303r@gmail.com>
2023-01-31 22:05:13 +0900

    Fix package.json and index.js

--- src/index.js
+++ src/index.js
@@ -1,27 +1,14 @@
 import { config } from "./config.js";
+import dotenv from "dotenv";
+import { Client, Events, GatewayIntentBits } from "discord.js";
 
-const dotenv = require("dotenv");
 dotenv.config();
-const {
-	Client,
-	Intents,
-	MessageActionRow,
-	MessageSelectMenu,
-	MessageButton,
-	Permissions,
-} = require("discord.js");
 
-const client = new Client({
-	intents: [
-		Intents.FLAGS.MESSAGE,
-		Intents.FLAGS.GUILDS,
-		Intents.FLAGS.GUILD_MESSAGES,
-	],
-});
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 client.on("ready", () => {
 	console.log(`${client.user.tag}がサーバーにログインしました！`);
 });
-client.on("interactionCreate", async (interaction) => {
+client.once(Events.InteractionCreate, async (interaction) => {
 	if (!interaction.isCommand()) return;
 	if (interaction.commandName === "hello") {
 		await interaction.reply("hello!");
