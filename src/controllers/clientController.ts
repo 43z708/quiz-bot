ff4114eba84fbfa3a17f87bbce12bfd4a2daceeb
43z708 <s7080303r@gmail.com>
2023-03-13 09:57:57 +0900

    Remove console

--- src/controllers/clientController.ts
+++ src/controllers/clientController.ts
@@ -59,19 +59,19 @@ export class ClientlController {
   public guildCreate(db: admin.firestore.Firestore) {
     this.client.on('guildCreate', async (guild) => {
       try {
-      if (this.client.application) {
-        // 開発環境ではsetの第2引数にguildIdを入れ、本番環境ではキャッシュさせるため第2引数は不要
-        await this.client.application.commands.set(Commands);
+        if (this.client.application) {
+          // 開発環境ではsetの第2引数にguildIdを入れ、本番環境ではキャッシュさせるため第2引数は不要
+          await this.client.application.commands.set(Commands);
+        }
+        await Promise.all([
+          ChannelController.create(guild, this.client.user?.id ?? '', db),
+          GuildController.create(guild, db),
+        ]);
+        this.channels = await ChannelController.getAllChannels(db);
+      } catch (e) {
+        console.log(e);
       }
-      await Promise.all([
-        ChannelController.create(guild, this.client.user?.id ?? '', db),
-        GuildController.create(guild, db),
-      ]);
-      this.channels = await ChannelController.getAllChannels(db);
-    } catch (e ) {
-      console.log(e)
-    }
-  });
+    });
   }
 
   /**
@@ -81,50 +81,48 @@ export class ClientlController {
   public interactionCreate(db: admin.firestore.Firestore) {
     this.client.on('interactionCreate', async (interaction) => {
       try {
-      // サーバー内のチャンネル情報を取得
-      const channels = this.channels.filter(
-        (channel) => channel.guildId === interaction.guild?.id
-      );
+        // サーバー内のチャンネル情報を取得
+        const channels = this.channels.filter(
+          (channel) => channel.guildId === interaction.guild?.id
+        );
 
-      // サーバー情報が読み取れない場合はエラー
-      if (!interaction.guildId && interaction.isRepliable()) {
-        await interaction.reply({
-          content: utils.mustUseInServer,
-        });
-        return;
+        // サーバー情報が読み取れない場合はエラー
+        if (!interaction.guildId && interaction.isRepliable()) {
+          await interaction.reply({
+            content: utils.mustUseInServer,
+          });
+          return;
+        }
+
+        // 送られたメッセージが所属するチャンネルがquizチャンネルかどうか
+        const isQuizChannel = ChannelController.isQuizChannel(
+          channels,
+          interaction.channelId
+        );
+
+        // クイズ開始(quizチャンネルのみ)
+        if (
+          interaction.type === InteractionType.ApplicationCommand &&
+          interaction.commandName === utils.quizStartCommandName &&
+          isQuizChannel
+        ) {
+          await QuizController.start(interaction, db);
+        }
+
+        // quizチャンネルのIDを取得
+        const channelId = ChannelController.getQuizChannel(channels).id;
+
+        if (
+          interaction.type === InteractionType.MessageComponent &&
+          interaction.isStringSelectMenu() &&
+          interaction.isRepliable() &&
+          interaction.channelId === channelId
+        ) {
+          await QuizController.reply(interaction, db);
+        }
+      } catch (e) {
+        console.log(e);
       }
-
-      // 送られたメッセージが所属するチャンネルがquizチャンネルかどうか
-      const isQuizChannel = ChannelController.isQuizChannel(
-        channels,
-        interaction.channelId
-      );
-
-      // クイズ開始(quizチャンネルのみ)
-      if (
-        interaction.type === InteractionType.ApplicationCommand &&
-        interaction.commandName === utils.quizStartCommandName &&
-        isQuizChannel
-      ) {
-        await QuizController.start(interaction, db);
-      }
-
-      // quizチャンネルのIDを取得
-      const channelId = ChannelController.getQuizChannel(channels).id;
-
-      if (
-        interaction.type === InteractionType.MessageComponent &&
-        interaction.isStringSelectMenu() &&
-        interaction.isRepliable() &&
-        interaction.channelId === channelId
-      ) {
-        await QuizController.reply(interaction, db);
-      }
-              
-    } catch (e) {
-      console.log(e)
-    }
-
     });
   }
 
@@ -150,7 +148,6 @@ export class ClientlController {
           });
           return;
         }
-        console.log({ message });
         // サーバー内のチャンネル情報を取得
         const channels = this.channels.filter(
           (channel) => channel.guildId === message.guild?.id
@@ -162,14 +159,12 @@ export class ClientlController {
             channels,
             message.channelId
           );
-        console.log({ isQuizManagementChannel });
 
         // 送られたメッセージが所属するチャンネルがquizチャンネルかどうか
         const isQuizChannel = ChannelController.isQuizChannel(
           channels,
           message.channelId
         );
-        console.log({ isQuizChannel });
 
         // csvテンプレート出力(quiz-managementチャンネルのみ)
         if (
